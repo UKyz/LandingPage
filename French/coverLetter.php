dafd0047159599f197c157e5a5ba64c0c12b85ba
UKyz <ukyz@icloud.com>
2018-05-26 18:35:57 +0200

    Add some texts

--- French/coverLetter.php
+++ French/coverLetter.php
@@ -36,10 +36,15 @@
 				<!-- Main -->
 					<section id="main" class="wrapper">
 						<div class="inner">
-							<h1 class="major">Ma lettre de motivation</h1>
+							<h1 class="major">Ma Lettre De Motivation</h1>
 							<span class="image fit"><img src="../images/resume.jpg" alt="" /></span>
-							<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique.</p>
-							<p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+							<p>Madame, Monsieur,</p>
+							<p>Actuellement en 4ème année en école d'ingénieur à l'ESME Sudria à Ivry-sur-seine, je suis à la recherche d'un stage de fin d'étude dans le domaine de la programmation informatique.</p>
+							<p>Après mes deux années de classe préparatoire j'ai choisie le parcours informatique de l'école. Grâce à ces premières années j'ai appris à travailler en groupe sur de nombreux projets mais aussi à être efficace et appliqué sur mon travail personnel. J'ai également eu la chance d'étudier un semestre à Dublin dans une filière informatique.</p>
+							<p>J'ai une certaine expèrience dans le web, J'ai déjà travaillé pour des clients qui demandaient une refonte de leurs site internet. C'est grâce à ce genre d'expèrience que je pense être dans la capacité d'apporté ma touche et de travaillé aisement avec vos clients.</p>
+							<p>Je pratique le football en club depuis mes 6 ans, j'ai un très bon esprit d'équipe et je sais facilement travailler en groupe. J'ai les capacités pour pousser tout un groupe à être plus performant mais aussi a prendre les commandes si le besoin de fait ressentir. Grâce à l'école et à mon temps extra-scolaire j'ai appris à coder dans différents langages mais aussi à modéliser et imprimer en 3D.</p>
+							<p>Je vous prie d’agréer, Madame, Monsieur, l’expression de mes salutations distinguées.</p>
+							<p>Victor Fauquembergue</p>
 						</div>
 					</section>
 
