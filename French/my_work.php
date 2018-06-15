275e5be4a039237b5cf3b1f32bbfbd0dc4a75a55
UKyz <ukyz@icloud.com>
2018-06-15 11:16:42 +0200

    Add text and image in my work

---
+++ French/my_work.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Mon Travail - Victor</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<link rel="stylesheet" href="../assets/css/zoe2.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<a href="resume.php" class="title">Fauquembergue Victor</a>
+				<nav>
+					<ul>
+						<li><a href="index.php">Accueil</a></li>
+						<li><a href="resume.php">Curriculum Vitae</a></li>
+						<li><a href="coverLetter.php">Lettre De Motivation</a></li>
+						<li><a href="my_work.php" class="active">Mon Travail</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<section id="main" class="wrapper">
+					<div class="inner">
+						<h1 class="major">Mon travail</h1>
+						<div class="grid row uniform">
+							<figure class="effect-zoe 6u">
+								<img src="../imagesGit/smurf1.jpeg" alt="Smurf-Hill"/>
+								<figcaption>
+									<h2><span>Smurf Hill</span></h2>
+									<p class="description">Un jeu en 2D de "Tower-Defense" basé sur les Schtroumpfs implémenté en Qt.</p>
+								</figcaption>			
+							</figure>
+							<figure class="effect-zoe 6u">
+								<img src="../imagesGit/princeton2.png" alt="Princeton Plainsboro"/>
+								<figcaption>
+									<h2><span>Princeton</span></h2>
+									<p class="description">Une application web PHP permettant de gérer une clinique. Gestion des docteurs, patients, réservations et l'intégralité des pages publiques.</p>
+								</figcaption>			
+							</figure>
+							<figure class="effect-zoe 6u">
+								<img src="../imagesGit/screenshot1.png" alt="TPSevenWonders"/>
+								<figcaption>
+									<h2><span>TPSevenWonders</span></h2>
+									<p class="description">Un jeu implémenté en JS, basé sur le jeu Seven Wonders. Un duel avec face à face deux cité qui gérent leurs ressources, unités et divinités.</p>
+								</figcaption>			
+							</figure>
+						</div>
+					</div>
+				</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Fauquembergue Victor. Tous droits reservés.</li><li>Switch in : <a href="../English/resume.php">English</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../assets/js/skel.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../assets/js/main.js"></script>
+
+			<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+  			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
